869cd43c2755928fcac30d47ca9808bc000419c0
Svencap <Ayzekmanduar@mail.ru>
2023-02-07 17:02:20 +0900

    add Header

---
+++ src/components/SubHeading/SubHeading.tsx
@@ -0,0 +1,15 @@
+import React, { FC } from "react";
+import { images } from "../../constants";
+
+interface SubHeadingProps {
+  title: string;
+}
+
+const SubHeading: FC<SubHeadingProps> = ({ title }) => (
+  <div style={{ marginBottom: "1rem" }}>
+    <p className="p__cormorant">{title}</p>
+    <img src={images.spoon} alt="spoon_image" className="spoon__img" />
+  </div>
+);
+
+export default SubHeading;
